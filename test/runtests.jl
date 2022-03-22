04ff83276210f71ac8f5aaf8d01a02d8acaf3f47
Quejiahao <quejiahao@live.com>
2022-03-22 12:58:52 +0800

    fix test tolerance

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,5 @@ using Test
 @testset "NumericalSolutionOfPartialDifferentialEquations.jl" begin
     @test test_solve_poissions_equation_known() < 0.0008358025108066159
     log2_err = test_solve_poissions_equation_unknown()[2]
-    @test abs(2 + log2_err[end] - log2_err[end-1]) < 0.00013088194455690429
+    @test abs(2 + log2_err[end] - log2_err[end-1]) < 0.00014
 end
